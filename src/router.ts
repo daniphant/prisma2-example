5f9723a65ad5b3e8630796ee8839b48f61095dba
daniphant <daniphant@protonmail.com>
2021-10-22 16:21:37 -0300

    feat: add getPosts useCase endpoints

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,11 @@
 import Router from "express";
-import authMiddleware from "./common/middlewares/auth";
-import { PostsRepository } from "./repositories/implementations/PostsRepository";
+
 import { authenticateController } from "./useCases/Authenticate";
 import { createPostController } from "./useCases/CreatePostUseCase";
 import { createUserController } from "./useCases/CreateUserUseCase";
+import { getPostController } from "./useCases/GetPostUseCase";
+
+import authMiddleware from "./common/middlewares/auth";
 
 const router = Router();
 
@@ -16,14 +18,10 @@ router.post("/authenticate", (req, res) =>
 router.use(authMiddleware);
 
 // PROTECTED
-router.get("/posts", async (req, res) => {
-  const postsRepository = new PostsRepository();
-
-  const posts = await postsRepository.findAll(req.userId as string);
-
-  return res.status(200).send(posts);
-});
-
+router.get("/posts", async (req, res) => getPostController.handle(req, res));
+router.get("/posts/:id", async (req, res) =>
+  getPostController.handleWithParam(req, res),
+);
 router.post("/posts", (req, res) => createPostController.handle(req, res));
 
 export { router };
